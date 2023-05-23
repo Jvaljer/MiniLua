d6fb5a880a10d6b9b7054bb4b26dc67bc4469350
Jvaljer <abbel.haol@gmail.com>
2023-05-23 21:19:52 +0200

    started to structure the interpreter

--- code/template/ocaml/interp/interp.ml
+++ code/template/ocaml/interp/interp.ml
@@ -5,26 +5,63 @@ type env = Value.env
 (* Fonction auxiliaire pour créer une table d'environnement à partir de noms et
    valeurs associées. *)
 let create_scope (names: string list) (values: value list) : (name, value) Hashtbl.t =
-  assert false
+  let scope = Hashtbl.create (List.length names) in
+  List.iter2 (fun name v -> Hashtbl.add scope name v) names values;
+  scope
 
 (* Fonctions de l'interprète, mutuellement récursives. Une fonction par
    catégorie syntaxique de l'AST. *)
 
 (* Interprète un bloc de code *)
 let rec interp_block (env : env) (blk : block) : value =
-  assert false
+  let loc_scp = create_scope blk.locals (List.map (fun _ -> Value.Nil) blk.locals) in
+  let env' = { env with locals = loc_scp::env.locals } in
+  let rec eval_stat = function 
+    (* must complete this part *)
+    | Nop                     -> ()
+    | Seq (s,s')              -> assert false
+    | Assign (v,e)            -> assert false
+    | FunctionCall fc         -> assert false 
+    | WhileDoEnd (cond, body) -> assert false
+    | If (cond, e, e')        -> assert false
+    | _ -> failwith "(interp_block)::(eval_stat)-> blk.body not matched" 
+  in
+  eval_stat blk.body;
+  interp_exp env' blk.ret
 
 (* Interprète un statement *)
 and interp_stat (env : env) (stat : stat) : unit =
-  assert false
+  match stat with
+    | Nop                     -> ()
+    | Seq (s,s')              -> assert false
+    | Assign (v,e)            -> assert false
+    | FunctionCall fc         -> assert false
+    | WhileDoEnd (cond, body) -> assert false
+    | If (cond, e, e')        -> assert false
+    | _ -> failwith "(interp_stat)-> stat not matched"
 
 (* Interprète un appel de fonction *)
 and interp_funcall (env : env) (fc : functioncall) : value =
-  assert false
+  match fc with 
+    | (fun_e, args) -> assert false
+    | _ -> failwith "(interp_funcall)-> fc not matched"
 
 (* Interprète une expression *)
 and interp_exp (env : env) (e : exp) : value =
- assert false
+  match e with 
+    | Nil                -> assert false
+    | False              -> assert false
+    | True               -> assert false
+    | Integer n          -> assert false
+    | Float n            -> assert false
+    | LiteralString str  -> assert false
+    | Var v              -> assert false
+    | FunctionCallE fc   -> assert false
+    | FunctionDef fb     -> assert false
+    | BinOp (bop, e, e') -> assert false
+    | UnOp (uop, e)      -> assert false
+    | Table elts         -> assert false
+    | _ -> failwith "(interp_exp)-> e not matched"
 
 let run ast =
   let globals = Hashtbl.create 47 in
