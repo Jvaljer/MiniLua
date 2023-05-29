53a77ad37d4668a58531e94fb3a5179ba322f224
Jvaljer <abbel.haol@gmail.com>
2023-05-29 19:01:50 +0200

    implemented basic Exp interpretations on Rust side (no test yet)

--- code/template/rust/src/interp/mod.rs
+++ code/template/rust/src/interp/mod.rs
@@ -44,15 +44,41 @@ impl Exp_ {
     fn interp<'ast, 'genv>(&'ast self, env: &mut Env<'ast, 'genv>) -> Value<'ast> {
         match self {
             Self::Nil => Value::Nil,
-            Self::False => unimplemented!(),
-            Self::True => unimplemented!(),
-            Self::Number(n) => unimplemented!(),
-            Self::LiteralString(str) => unimplemented!(),
-            Self::Var(var) => unimplemented!(),
+            Self::False => Value::Bool(false),
+            Self::True => Value::Bool(true),
+            Self::Number(n) => Value::Number(n),
+            Self::LiteralString(str) => Value::String(str),
+            Self::Var(var) => { match var {
+                                    Var::Name(name) => env.lookup(name),
+                                    Var::IndexTable(tab, k) => { let table = tab.interp(env).as_table();
+                                                                 let key = k.interp(env).as_table_key();
+                                                                 let val = table.borrow().get(&key);
+                                                                 match val {
+                                                                    Some(x) => x,
+                                                                    None => Value::Nil,
+                                                                 }
+                                                               }
+                                }
+                              },
             Self::ExpFunctionCall(fc) => unimplemented!(),
             Self::FunctionDef(fb) => unimplemented!(),
-            Self::BinOp(bop, e, e_) => unimplemented!(),
-            Self::Unop(uop, e) => unimplemented!(),
+            Self::BinOp(bop, e, e_) => match bop {
+                                           Binop::Addition => { unimplemented!() }
+                                           Binop::Subtraction => { unimplemented!() }
+                                           Binop::Multiplication => { unimplemented!() }
+                                           Binop::Equality => { unimplemented!() }
+                                           Binop::Inequality => { unimplemented!() }
+                                           Binop::Less => { unimplemented!() }
+                                           Binop::LessEq => { unimplemented!() }
+                                           Binop::Greater => { unimplemented!() }
+                                           Binop::GreaterEq => { unimplemented!() }
+                                           Binop::LogicalAnd => { unimplemented!() }
+                                           Binop::LogicalOr => { unimplemented!() }
+                                       },
+            Self::Unop(uop, e) => match uop {
+                                      Unop::UnaryMinus => { unimplemented!() }
+                                      Unop::Not => { unimplemented!() }
+                                  },
             Self::Table(tab) => unimplemented!(),
         }
     }
