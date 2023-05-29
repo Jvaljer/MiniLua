a20b6cfb04762dd1b8de03df77762efb85db5dac
Jvaljer <abbel.haol@gmail.com>
2023-05-29 09:04:04 +0200

    started to do the mod.rs interpreter architecture

--- code/template/rust/src/interp/mod.rs
+++ code/template/rust/src/interp/mod.rs
@@ -3,7 +3,7 @@ use self::{
     value::{Value, Function},
 };
 use crate::parser::ast::*;
-use std::{rc::Rc, collections::HashMap};
+use std::{rc::Rc, collections::HashMap, cell::RefCell, borrow::BorrowMut};
 
 mod env;
 pub mod value;
@@ -11,14 +11,24 @@ pub mod value;
 impl Block {
     // Interprétation d'un bloc
     fn interp<'ast, 'genv>(&'ast self, env: &mut Env<'ast, 'genv>) -> Value<'ast> {
-        unimplemented!()
+        let vec = vec![Value::Nil; self.locals.len()];
+        let mut env_ = Env { locals: env.locals.extend(&self.locals, vec.into_iter()), globals: &mut env.globals, };
+        self.body.interp(&mut env_);
+        self.ret.interp(&mut env_)
     }
 }
 
 impl Stat_ {
     // Interprétation d'une instruction
     fn interp<'ast, 'genv>(&'ast self, env: &mut Env<'ast, 'genv>) -> () {
-        unimplemented!()
+        match self {
+            Self::Nop => (),
+            Self::Seq(e,e_) => { unimplemented!() }
+            Self::StatFunctionCall(fc) => { unimplemented!() }
+            Self::Assign(val, e) => { unimplemented!() }
+            Self::WhileDoEnd(cond, e) => { unimplemented!() }
+            Self::If(cond, e, e_) => { unimplemented!() }
+        }
     }
 }
 
@@ -32,7 +42,19 @@ impl FunctionCall {
 impl Exp_ {
     // Interprétation d'une expression
     fn interp<'ast, 'genv>(&'ast self, env: &mut Env<'ast, 'genv>) -> Value<'ast> {
-        unimplemented!()
+        match self {
+            Self::Nil => Value::Nil,
+            Self::False => unimplemented!(),
+            Self::True => unimplemented!(),
+            Self::Number(n) => unimplemented!(),
+            Self::LiteralString(str) => unimplemented!(),
+            Self::Var(var) => unimplemented!(),
+            Self::ExpFunctionCall(fc) => unimplemented!(),
+            Self::FunctionDef(fb) => unimplemented!(),
+            Self::BinOp(bop, e, e_) => unimplemented!(),
+            Self::Unop(uop, e) => unimplemented!(),
+            Self::Table(tab) => unimplemented!(),
+        }
     }
 }
 
