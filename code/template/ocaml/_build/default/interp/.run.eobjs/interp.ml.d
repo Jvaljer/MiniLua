4c392394ea8a767b6e953fd7ae28b2f108a9d4a5
Jvaljer <abbel.haol@gmail.com>
2023-06-06 16:35:19 +0200

    finished OCaml side HALLELUJAH -> if enough time starting interp-cps after report...

--- code/template/ocaml/_build/default/interp/.run.eobjs/interp.ml.d
+++ code/template/ocaml/_build/default/interp/.run.eobjs/interp.ml.d
@@ -1,1 +1,1 @@
-interp/interp.ml: Hashtbl List Luaparser Value
+interp/interp.ml: Hashtbl List Luaparser String Value
--- code/template/ocaml/interp/interp.ml
+++ code/template/ocaml/interp/interp.ml
@@ -5,123 +5,171 @@ type env = Value.env
 (* Fonction auxiliaire pour créer une table d'environnement à partir de noms et
    valeurs associées. *)
 let create_scope (names: string list) (values: value list) : (name, value) Hashtbl.t =
-  (*Printf.printf "create_scope";*)
-  let scope = Hashtbl.create (List.length names) in
-  List.iter2 (fun name v -> Hashtbl.add scope name v) names values;
-  (*Invalid_arg ("List.iter2") ???*)
-  scope
+  let scope = Hashtbl.create 10 in
+  let rec loop names values =
+    match names, values with
+      | [], _ -> ()
+      | name::names, value::values ->
+        Hashtbl.replace scope name value;
+        loop names values
+      | n::names, [] ->
+        Hashtbl.replace scope n Value.Nil;
+        loop names []
+    in
+    loop names values;
+    scope
 
 (* Fonctions de l'interprète, mutuellement récursives. Une fonction par
    catégorie syntaxique de l'AST. *)
 
 (* Interprète un bloc de code *)
 let rec interp_block (env : env) (blk : block) : value =
-  (*Printf.printf "interp_block";*)
-  (match env.locals with 
+  (match env.locals with
     | [] -> ()
-    | loc::_ -> List.iter (fun n -> Hashtbl.replace loc n Value.Nil) blk.locals);
+    | local :: _ -> List.iter (fun name -> Hashtbl.replace local name Value.Nil) blk.locals
+  );
   interp_stat env blk.body;
   interp_exp env blk.ret
 
 (* Interprète un statement *)
 and interp_stat (env : env) (stat : stat) : unit =
-  (*Printf.printf "interp_stat";*)
   match stat with
-    | Nop                  -> ()
-    | Seq (s, s')          -> interp_stat env s; 
-                              interp_stat env s'
-    | Assign (var, e)      -> let v = interp_exp env e in
-                              ( match var with 
-                                  | Name name         -> Value.set_ident env name v
-                                  | IndexTable (_, i) -> let i_val = interp_exp env i in
-                                                         let table = Value.as_table i_val in
-                                                         let i_key = Value.as_table_key i_val in
-                                                         Hashtbl.replace table i_key v
-                              )
-    | FunctionCall fc      -> ignore (interp_funcall env fc)
-    | WhileDoEnd (e, body) -> if Value.as_bool (interp_exp env e) then
-                                interp_stat env body;
-                                interp_stat env stat
-
-    | If (cond, b, b')     -> if Value.as_bool (interp_exp env cond) then
-                                interp_stat env b 
-                              else
-                                interp_stat env b'
+    | Nop -> ()
+    | Seq (s, s') -> 
+      interp_stat env s; 
+      interp_stat env s'
+    | Assign (var, e) -> 
+      (*cannot interpret the value this early unfortunately*)
+      (*let value = interp_exp env e in*)
+      ( match var with
+          | Name name ->
+            let value = interp_exp env e in
+            Value.set_ident env name value
+          | IndexTable(t, k) ->
+            let tab = interp_exp env t in
+            let table = Value.as_table tab in
+            let k' = interp_exp env k in
+            let key = Value.as_table_key k' in 
+            (*cannot interpret the value before this moment*)
+            let value = interp_exp env e in
+            Hashtbl.replace table key value
+      )
+    | FunctionCall fc -> ignore (interp_funcall env fc)
+    | WhileDoEnd (cond, body) ->
+      let c = interp_exp env cond in
+      if Value.as_bool c then 
+        ( 
+          interp_stat env body;
+          interp_stat env stat 
+        )
+    | If (cond, s, s') -> 
+      let c = interp_exp env cond in
+      if Value.as_bool c then 
+        interp_stat env s
+      else 
+        interp_stat env s'
 
 (* Interprète un appel de fonction *)
 and interp_funcall (env : env) (fc : functioncall) : value =
-  (*Printf.printf "interp_funcall";*)
   match fc with 
-    | (f_exp, args) ->
-      let f_val = interp_exp env f_exp in
-      let a_vals = List.map (interp_exp env) args in
-      ( match f_val with 
-          | Value.Function f -> ( match f with 
-                                    | Value.Print -> 
-                                      List.iter (fun arg_val -> print_string (Value.to_string arg_val ^ "\t")) a_vals;
-                                      print_newline ();
-                                      Value.Nil
-                                    | Value.Closure (names, clos_env, body) -> 
-                                      let scope' = create_scope names a_vals in
-                                      let env' = { clos_env with locals = scope'::clos_env.locals } in
-                                      interp_block env' body
-                                )
-          (*called for 21*)
-          | _ -> failwith "(interp_funcall)-> fc isn't a function value"
+    | (f_e, a) ->
+      let f_v = interp_exp env f_e in
+      let args = List.map (interp_exp env) a in
+      ( match (Value.as_function f_v) with
+          | Value.Print -> 
+            (* commented is working as values printed are the good ones, but compiler didn't like it enough to say [OK]...
+               so I tried something else which was accepted *)
+            (*List.iter (fun a_v -> print_string (Value.to_string a_v ^ "\t")) args;
+            print_newline (); *)
+            let a_s = List.map (fun a' -> Value.to_string a') args in
+            print_endline (String.concat "\t" a_s);
+            Value.Nil
+          | Value.Closure (names,clos_env,body) ->
+            interp_block { clos_env with locals = (create_scope names args)::clos_env.locals } body
       )
+
+(* interprète table *)
+and interp_table (env : env) (l : (exp * exp) list ) : value=
+  let table = Hashtbl.create 10 in
+  List.iter (fun (key, v) -> 
+    let key = Value.as_table_key (interp_exp env key) in
+    let value = interp_exp env v in
+    Hashtbl.replace table key value) l;
+  Value.Table table
 
 (* Interprète une expression *)
 and interp_exp (env : env) (e : exp) : value =
-  (*Printf.printf "interp_exp";*)                              
-  match e with 
-    | Nil                     -> Value.Nil
-    | False                   -> Value.Bool false
-    | True                    -> Value.Bool true
-    | Integer n               -> Value.Int n
-    | Float f                 -> Value.Float f
-    | LiteralString str       -> Value.String str
-    | Var v                   -> ( match v with
-                                     | Name name -> Value.lookup_ident env name
-                                     (*called for18,19,24*)
-                                     | _ -> failwith "(interp_exp)::(Var)-> var isn't 'Name'"
-                                 )
-    | FunctionCallE fc        -> interp_funcall env fc
-    | FunctionDef (name, blk) -> Value.Function (Value.Closure (name, env, blk))
-    | BinOp (bop, e, e')      -> let v = interp_exp env e in
-                                 let v' = interp_exp env e' in
-                                 ( match bop with 
-                                   | Addition       -> Value.add v v'
-                                   | Subtraction    -> Value.sub v v'
-                                   | Multiplication -> Value.mul v v'
-                                   | Equality       -> Value.Bool (Value.equal v v')
-                                   | Inequality     -> Value.Bool (not (Value.equal v v'))
-                                   | Less           -> Value.Bool (Value.lt v v')
-                                   | LessEq         -> Value.Bool (Value.le v v')
-                                   | Greater        -> Value.Bool (not (Value.le v v'))
-                                   | GreaterEq      -> Value.Bool (not (Value.lt v v'))
-                                   | LogicalAnd     -> ( match (v,v') with 
-                                                           | Value.Bool false, _              -> v
-                                                           | _, Value.Bool false              -> v'
-                                                           | Value.Bool true, Value.Bool true -> v
-                                                           (*called on 11*)
-                                                           | _,_ -> failwith "(interp_exp)::(BinOp)::(LogicalAnd)-> {v,v'} not matched"
-                                                       )
-                                   | LogicalOr      -> ( match (v,v') with 
-                                                           | Value.Bool false, Value.Bool false -> v
-                                                           | Value.Bool _, Value.Bool _         -> Value.Bool true
-                                                           | _,_ -> failwith "(interp_exp)::(BinOp)::(LogicalOr)-> {v,v'} not matched"
-                                                       )
-                                 )
-    | UnOp (uop, e)            -> let v = interp_exp env e in
-                                  ( match uop with 
-                                      | UnaryMinus -> (* Value.mul (Value.Int Int64.minus_one) v *) Value.neg v  
-                                      | Not        -> Value.Bool (not (Value.as_bool v))
-                                  )
-    | Table elts               -> let tab = Hashtbl.create 16 in
-                                  List.iter (fun (k,v) -> let key = Value.as_table_key (interp_exp env k) in
-                                                          let value = interp_exp env v in
-                                                          Hashtbl.replace tab key value) elts;
-                                  Value.Table tab
+ match e with
+  | Nil -> Value.Nil
+  | False -> Value.Bool false
+  | True -> Value.Bool true
+  | Integer n -> Value.Int n
+  | Float f -> Value.Float f
+  | LiteralString s -> Value.String s
+  | Var var -> ( 
+    match var with
+      | Name n -> Value.lookup_ident env n
+      | IndexTable (t, k) -> 
+        let tab = interp_exp env t in 
+        let table = Value.as_table tab in
+        let k' = interp_exp env k in
+        let key = Value.as_table_key k' in
+        ( match Hashtbl.find_opt table key with
+            | Some v -> v
+            | None -> Value.Nil
+        )
+    )
+  | FunctionCallE fc -> interp_funcall env fc
+  | FunctionDef (name, blk) -> 
+    let clos = Value.Closure (name, env, blk) in
+    Value.Function clos
+  | BinOp (bop, e, e') -> (
+    match bop with 
+      | LogicalAnd -> 
+        let v = interp_exp env e in 
+        if Value.as_bool v then 
+          let v' = interp_exp env e' in
+          v'
+        else
+          v
+      | LogicalOr -> 
+        let v = interp_exp env e in 
+        if Value.as_bool v then 
+          v
+        else
+          let v' = interp_exp env e' in
+          v'
+      | _ -> 
+        let v = interp_exp env e in
+        let v' = interp_exp env e' in
+        ( match bop with 
+            | Addition -> Value.add v v'
+            | Subtraction -> Value.sub v v'
+            | Multiplication -> Value.mul v v'
+            | Equality -> Value.Bool (Value.equal v v')
+            | Inequality -> Value.Bool (not(Value.equal v v'))
+            | Less -> Value.Bool (Value.lt v v')
+            | LessEq -> Value.Bool (Value.le v v')
+            | Greater -> Value.Bool (not(Value.le v v'))
+            | GreaterEq -> Value.Bool (not(Value.lt v v'))
+            | _ -> failwith "binary operand not referenced in Lua"
+        )
+  )
+  | UnOp (uop, e) -> 
+    let v = interp_exp env e in
+    ( match uop with 
+        | UnaryMinus -> Value.neg v
+        | Not -> Value.Bool (not(Value.as_bool v))
+    )
+  | Table elts -> 
+    let table = Hashtbl.create 16 in
+    List.iter (fun (k,v) -> 
+      let k' = interp_exp env k in 
+      let key = Value.as_table_key k' in
+      let value = interp_exp env v in
+      Hashtbl.replace table key value
+    ) elts;
+    Value.Table table
 
 let run ast =
   let globals = Hashtbl.create 47 in
